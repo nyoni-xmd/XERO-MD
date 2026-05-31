6da375506f36d9c8512ce2807f0e81459338347c
nyoni-xmd <xmdnyoni@gmail.com>
2026-05-31 22:14:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,83 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-const config = {
-    PREFIX: process.env.PREFIX || '.',
-    MODE: process.env.MODE || 'public',
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || 'true',
-    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || 'true',
-    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || 'true',
-    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || 'false',
-    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || 'Thanks for status!',
-    READ_MESSAGE: process.env.READ_MESSAGE || 'true',
-    AUTO_REACT: process.env.AUTO_REACT || 'false',
-    CUSTOM_REACT: process.env.CUSTOM_REACT || 'false',
-    SESSION_ID: process.env.SESSION_ID || '',
-    DEV: process.env.DEV || '255763111390'
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
 }
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "",
+    // add your Session Id 
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+    // make true or false status auto seen
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    // make true if you want auto reply on status 
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    // make true if you want auto reply on status 
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*sᴇᴇɴ ʏᴏᴜʀ sᴛᴀᴛᴜs XERO-MD 🐺*",
+    // set the auto reply massage on status reply  
+    WELCOME: process.env.WELCOME || "false",
+    // true if want welcome and goodbye msg in groups    
+    ADMIN_EVENTS: process.env.ADMIN_EVENTS || "true",
+    // make true to know who dismiss or promoted a member in group
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    // make anti link true,false for groups 
+    MENTION_REPLY: process.env.MENTION_REPLY || "false",
+    // make true if want auto voice reply if someone menetion you 
+    MENU_IMAGE_URL: (process.env.MENU_IMAGE_URL || "https://files.catbox.moe/9gl0l8.jpg").trim(),
+    // add custom menu and mention reply image url
+    PREFIX: process.env.PREFIX || ".",
+    // add your prifix for bot   
+    BOT_NAME: process.env.BOT_NAME || "XERO-MD",
+    // add bot namw here for menu
+    STICKER_NAME: process.env.STICKER_NAME || "XERO-MD",
+    // type sticker pack name 
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    // make this true for custum emoji react    
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+    // chose custom react emojis by yourself 
+    DELETE_LINKS: process.env.DELETE_LINKS || "true",
+    // automatic delete links witho remove member 
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "255763111390,255610209120",
+    // add your bot owner numbers
+    OWNER_NAME: process.env.OWNER_NAME || "XERO-MD",
+    // add bot owner name
+    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ XERO-MD*",
+    // add bot owner name    
+    ALIVE_IMG: (process.env.ALIVE_IMG || "https://files.catbox.moe/a97zm1.jpg").trim(),
+    // add img for alive msg
+    LIVE_MSG: process.env.LIVE_MSG || "> *XERO-MD always online ❄️*",
+    // add alive msg here 
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    // Turn true or false for automatic read msgs
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    // make this true or false for auto react on all msgs
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    // false or true for anti bad words  
+    MODE: process.env.MODE || "public",
+    // make bot public-private-inbox-group 
+    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+    // make anti link true,false for groups 
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    // make true for send automatic voices
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    // make true for automatic stickers 
+    AUTO_REPLY: process.env.AUTO_REPLY || "",
+    // make true or false automatic text reply 
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    // maks true for always online 
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
+    // make false if want private mod
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    // true for automatic show typing   
+    READ_CMD: process.env.READ_CMD || "true",
+    // false if want mark commands as read 
+    DEV: process.env.DEV || "255763111390",
+    //replace with your whatsapp number        
+    ANTI_VV: process.env.ANTI_VV || "true",
+    // true for anti once view 
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+    // change it to 'same' if you want to resend deleted message in same chat 
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+    // make it true for auto recoding 
+};
