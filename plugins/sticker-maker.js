9069ee29346ffc7dbc09bbf0cb57c40e78a2de25
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-10 15:22:03 +0300

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "Diana";
+        let pack = Config.STICKER_NAME || "nyoni";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
