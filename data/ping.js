4a88b903c0ce64294173f81a6b181ea7da4d5634
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-13 15:50:31 +0300

    Delete data directory

--- data/ping.js
+++
@@ -1,2 +0,0 @@
-const { cmd } = require('../lib/functions');
-cmd({ pattern: "ping", desc: "Check bot response", category: "info", react: "🏓", filename: __filename }, async (conn, mek, m, { reply }) => { reply('🏓 Pong! Bot is alive ✅'); });
--- data/sticker.js
+++
@@ -1,9 +0,0 @@
-const { cmd } = require('../lib/functions');
-const { Sticker } = require('wa-sticker-formatter');
-cmd({ pattern: "sticker", alias: ["s"], desc: "Convert image to sticker", category: "convert", react: "🎴", filename: __filename }, async (conn, mek, m, { from, reply, quoted }) => {
-    let media = mek.message.imageMessage || (quoted?.message?.imageMessage);
-    if (!media) return reply("Reply to an image.");
-    let buffer = await conn.downloadMediaMessage(media);
-    let sticker = new Sticker(buffer, { pack: "XERO-MD", author: "nyoni-xmd" });
-    await conn.sendMessage(from, { sticker: await sticker.toBuffer() }, { quoted: mek });
-});
