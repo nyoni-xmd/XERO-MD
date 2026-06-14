4c24f3bf5c0026845ec9961baf313316004f01d4
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-14 16:14:07 +0300

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,2 +1,12 @@
-async function saveMessage(m) { return true; }
+const fs = require('fs');
+const path = require('path');
+
+const DB_FILE = path.join(__dirname, 'messages.json');
+
+async function saveMessage(m) {
+    // Simple logging - you can expand later
+    console.log(`💾 Message saved from ${m.key?.remoteJid}`);
+    return true;
+}
+
 module.exports = { saveMessage };
