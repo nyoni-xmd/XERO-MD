4d8f30afd9b3ee44bbeb652c637943479a1b7ccd
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-14 10:12:43 +0300

    Refactor owner command registration

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,2 +1,9 @@
-const { cmd } = require('../lib/functions');
-cmd({ pattern: "owner", alias: ["creator", "dev"], desc: "Owner info", category: "info", react: "👑", filename: __filename }, async (conn, mek, m, { reply }) => { reply(`👑 OWNER\nName: nyoni-xmd\nNumber: +255763111390\nNumber 2: +255610209120\nBot: XERO-MD`); });
+global.registerCommand({
+    command: "owner",
+    alias: ["creator", "dev"],
+    desc: "Owner information",
+    category: "info",
+    function: async (conn, m, store, { reply }) => {
+        reply("👑 *OWNER*\nName: nyoni-xmd\nNumber: +255763111390\nNumber 2: +255610209120\nBot: XERO-MD");
+    }
+});
