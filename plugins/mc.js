8ee216fce7353305d2a14f6f18f99a0f34dc7461
popkidbots <hillaryboiywo@gmail.com>
2026-06-15 15:38:28 +0300

    Delete plugins/mc.js

--- plugins/mc.js
+++
@@ -1,9 +0,0 @@
-global.registerCommand({
-    command: "mycmd",
-    alias: ["mc"],
-    desc: "Description of my command",
-    category: "tools",  // ← Hii category itaamua itaonekana wapi kwenye menu
-    function: async (conn, m, { reply }) => {
-        await reply("Command executed!");
-    }
-});
