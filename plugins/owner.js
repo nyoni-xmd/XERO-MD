2f76ad0b87f4a4457bd61661e118c5650d5fa141
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 12:22:28 +0300

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,17 @@
+global.registerCommand({
+    command: "owner",
+    alias: ["creator", "dev"],
+    desc: "Owner information",
+    category: "info",
+    function: async (conn, m, { reply }) => {
+        reply(`👑 *OWNER INFORMATION*
+╭━━━━━━━━━━━━━━━╮
+│ Name: nyoni-xmd
+│ Number: +255763111390
+│ Number 2: +255610209120
+│ Bot: XERO-MD
+╰━━━━━━━━━━━━━━━╯
+
+💬 *Bot is PUBLIC* - Anyone can use!`);
+    }
+});
