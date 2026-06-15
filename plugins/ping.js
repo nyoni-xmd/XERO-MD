ea9be854014a957cabdd85d932a759db08284a22
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 13:56:37 +0300

    Update ping command response message

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,10 +1,13 @@
-// plugins/ping.js
 global.registerCommand({
     command: "ping",
     alias: ["p"],
     desc: "Check bot response",
     category: "info",
     function: async (conn, m, { reply }) => {
-        await reply("🏓 Pong! Bot is alive and responding.");
+        await reply(`╭┈┈❍ *XERO-MD* ❍
+┊• *Pong! Bot is alive!*
+╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⭘
+
+> POWERED BY nyoni-xmd`);
     }
 });
