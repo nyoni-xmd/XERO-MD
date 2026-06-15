e58118b6536f42f4bc12a1aa9ef48657c786fce5
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 17:20:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "",
     PREFIX: process.env.PREFIX || ".",
     MODE: process.env.MODE || "public",
-    READ_MESSAGE: process.env.READ_MESSAGE || "false",
-    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY XERO-MD*",
-    BOT_NAME: process.env.BOT_NAME || "XERO-MD",
-    OWNER_NAME: process.env.OWNER_NAME || "nyoni-xmd",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "255763111390,255610209120",
-    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/gyaka2.png"
+    BOT_NAME: "XERO-MD",
+    OWNER_NAME: "nyoni-xmd",
+    OWNER_NUMBER: "255763111390,255610209120"
 };
