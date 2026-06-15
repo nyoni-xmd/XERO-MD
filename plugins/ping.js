46dcb3eb9e3bcbc22d794ba58fc689dcdc6d7182
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 14:38:04 +0300

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,13 +1,30 @@
+// plugins/ping.js
 global.registerCommand({
     command: "ping",
     alias: ["p"],
     desc: "Check bot response",
     category: "info",
     function: async (conn, m, { reply }) => {
-        await reply(`╭┈┈❍ *XERO-MD* ❍
-┊• *Pong! Bot is alive!*
+        const start = Date.now();
+        const msg = await reply("🏓");
+        const end = Date.now();
+        const ping = end - start;
+        
+        await conn.sendMessage(m.key.remoteJid, {
+            text: `╭┈┈❍ *XERO-MD* ❍
+┊• *Pong!* : ${ping}ms
 ╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⭘
 
-> POWERED BY nyoni-xmd`);
+> POWERED BY nyoni-xmd`,
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363399470975987@newsletter',
+                    newsletterName: 'XERO-MD',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: msg });
     }
 });
