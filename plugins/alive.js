459c6d38b38bda0ebaeeb8aa62d0738a79f49639
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 14:38:42 +0300

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -8,7 +8,7 @@ global.registerCommand({
     alias: ["alive"],
     desc: "Check if bot is active",
     category: "info",
-    function: async (conn, m, { from, reply }) => {
+    function: async (conn, m, { from, reply, sender }) => {
         const currentTime = moment().tz("Africa/Dar_es_Salaam").format("HH:mm:ss");
         const currentDate = moment().tz("Africa/Dar_es_Salaam").format("dddd, MMMM Do YYYY");
         
@@ -17,17 +17,20 @@ global.registerCommand({
         const minutes = Math.floor((runtime % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((runtime % (1000 * 60)) / 1000);
 
-        const aliveText = `╭─ 「 *XERO-MD* 」
-│✨ Bot is active & online!
-│🧠 Owner: nyoni-xmd
-│📞 Number 1: +255763111390
-│📞 Number 2: +255610209120
-│⚡ Version: 3.0.0
-│🕒 Time: ${currentTime}
-│📳 Mode: public
-│📅 Date: ${currentDate}
-│⏳ Uptime: ${hours}h ${minutes}m ${seconds}s
-╰────────────────❍
+        let senderNumber = sender ? sender.split('@')[0] : "User";
+
+        const aliveText = `╭┈┈❍ *XERO-MD* ❍
+┊• *Bot is active & online!*
+┊• *User* : @${senderNumber}
+┊• *Owner* : nyoni-xmd
+┊• *Number 1* : +255763111390
+┊• *Number 2* : +255610209120
+┊• *Version* : 3.0.0
+┊• *Time* : ${currentTime}
+┊• *Mode* : public
+┊• *Date* : ${currentDate}
+┆• *Uptime* : ${hours}h ${minutes}m ${seconds}s
+╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⭘
 
 > POWERED BY nyoni-xmd
 ⚡ POWER - SPEED - CONTROL
@@ -35,7 +38,17 @@ global.registerCommand({
 
         await conn.sendMessage(from, {
             image: { url: 'https://files.catbox.moe/gyaka2.png' },
-            caption: aliveText
+            caption: aliveText,
+            contextInfo: {
+                mentionedJid: [`${senderNumber}@s.whatsapp.net`],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363399470975987@newsletter',
+                    newsletterName: 'XERO-MD',
+                    serverMessageId: 143
+                }
+            }
         }, { quoted: m });
     }
 });
