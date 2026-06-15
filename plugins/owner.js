f8fa37c84fea6b71025a7370c05945125c6a2956
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 13:56:12 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,13 +1,15 @@
-// plugins/owner.js
 global.registerCommand({
     command: "owner",
     alias: ["creator", "dev"],
     desc: "Owner info",
     category: "info",
     function: async (conn, m, { reply }) => {
-        await reply(`👑 OWNER
-Name: nyoni-xmd
-Number: +255763111390
-Number 2: +255610209120`);
+        await reply(`╭┈┈❍ *XERO-MD* ❍
+┊• *Owner* : nyoni-xmd
+┊• *Number 1* : +255763111390
+┊• *Number 2* : +255610209120
+╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈⭘
+
+> POWERED BY nyoni-xmd`);
     }
 });
