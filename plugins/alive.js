06f2fac7d1ee296df9bcf39215608ddbee8c0c96
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-15 12:22:04 +0300

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,8 @@
+global.registerCommand({
+    command: "alive",
+    desc: "Check if bot is running",
+    category: "info",
+    function: async (conn, m, { reply }) => {
+        reply("✨ XERO-MD is alive and online!\n⚡ Power - Speed - Control\n🚀 Beyond Limits");
+    }
+});
