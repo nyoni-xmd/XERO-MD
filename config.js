c2810ac51f7d3c1126d7231ca7afbd0e33756ccc
nyoni-xmd <xmdnyoni@gmail.com>
2026-06-16 11:08:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,31 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
+// config.js
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "",
     PREFIX: process.env.PREFIX || ".",
     MODE: process.env.MODE || "public",
-    BOT_NAME: "XERO-MD",
-    OWNER_NAME: "nyoni-xmd",
-    OWNER_NUMBER: "255763111390,255610209120"
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "👀 Status viewed!",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+    BOT_NAME: process.env.BOT_NAME || "XERO-MD",
+    OWNER_NAME: process.env.OWNER_NAME || "nyoni-xmd",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "255763111390,255610209120",
+    DEV: process.env.DEV || "255763111390",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    ANTI_DELETE: process.env.ANTI_DELETE || "true",
+    ANTI_CALL: process.env.ANTI_CALL || "true",
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    WELCOME: process.env.WELCOME || "true",
+    GOODBYE: process.env.GOODBYE || "true",
+    ADMIN_ACTION: process.env.ADMIN_ACTION || "true",
+    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴʏᴏɴɪ-xᴍᴅ*",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/gyaka2.png",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gyaka2.png",
+    LIVE_MSG: process.env.LIVE_MSG || "> *XERO-MD always  ⚡*",
+    AUTO_TYPING: process.env.AUTO_TYPING || "true",
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 };
